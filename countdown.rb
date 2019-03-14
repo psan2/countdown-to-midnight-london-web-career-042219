1239dca454fc1137d1658de18e027e495b26f810
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-14 17:26:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
-require 'pry'
-binding.pry
 def countdown(remaining_seconds)
-  binding.pry
   while remaining_seconds > 0
     puts "#{remaining_seconds} SECOND(S)!"
     remaining_seconds -= 1
   end
   "HAPPY NEW YEAR!"
 end
+
+countdown(100)
