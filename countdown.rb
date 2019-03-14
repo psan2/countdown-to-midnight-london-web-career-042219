340a979a14d231b5c46d6cf5c6e8cd81c1f861e1
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-14 17:17:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,8 @@
 #write your code here
+require 'pry'
 
 def countdown(remaining_seconds)
+  binding.pry
   while remaining_seconds > 0
     puts "#{remaining_seconds} SECOND(S)!"
     remaining_seconds -= 1
