b75cc4405acbc316405cb10f885aecd201c2ae30
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-14 17:11:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(remaining_seconds)
+  while remaining_seconds > 0
+    puts "#{remaining_seconds} SECOND(S)!"
+    remaining_seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
