752bbc13ca739938c645cfe890034d3b8294663c
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-14 17:28:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown(remaining_seconds)
+  while remaining_seconds > 0
+    puts "#{remaining_seconds} SECOND(S)!"
+    remaining_seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(remaining_seconds)
+  while remaining_seconds > 0
+    puts "#{remaining_seconds} SECOND(S)!"
+    remaining_seconds -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
